ff2554429918e76026cd5d853225b7b9f95dd346
BekturMuratov <Muratov225@gmail.com>
2020-03-01 15:18:30 +0600

    food delivery app

---
+++ frontend/components/Header.vue
@@ -0,0 +1,16 @@
+<template>  
+  <client-only>
+    <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-left">
+            <ul class="uk-navbar-nav">
+              <li class="uk-active">
+                <router-link tag="a" class="navbar-brand" to="/" exact>Deliveroo clone</router-link>
+              </li>
+              <li>
+                <router-link tag="a" class="navbar-brand" to="/restaurants" exact>Restaurants</router-link>
+              </li>
+            </ul>
+        </div>
+    </nav>
+  </client-only>
+</template>
